2970ce20a40349ba993cb201830cecc0a4015e5e
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2021-08-19 11:01:20 +0100

    Initial Commit

---
+++ TimeZoner/TimeZonerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZonerApp.swift
+//  TimeZoner
+//
+//  Created by tarrask on 19/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeZonerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
