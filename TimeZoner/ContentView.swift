08b135845f29ab7f9ae2f1ed92514e54f0644518
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2021-08-19 16:36:54 +0100

    Created AddPerson View

--- TimeZoner/ContentView.swift
+++ TimeZoner/ContentView.swift
@@ -9,16 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        let manager = TimeManager()
-        let testPersons: [Person] = [
-            Person(name: "SambaRock", timezone: TimeZone(identifier: "America/New_York") ?? TimeZone(identifier: "GMT")!),
-            Person(name: "Cotey", timezone: TimeZone(identifier: "America/Chicago") ?? TimeZone(identifier: "GMT")!),
-            Person(name: "Mikee", timezone: TimeZone(identifier: "America/Los_Angeles") ?? TimeZone(identifier: "GMT")!)
-        ]
-        
-        manager.persons = testPersons
-        
-        return Watch(manager: manager)
+        AddPersonView()
     }
 }
 
